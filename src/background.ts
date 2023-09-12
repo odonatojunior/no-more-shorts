fa386430d6cd330b01e0d1269289a1997f4770fb
Donato Jr <donato.junior.07@gmail.com>
2023-09-11 23:24:39 -0300

    feat: remove onInstalled event listener

--- src/background.ts
+++ src/background.ts
@@ -3,40 +3,38 @@ function convertShortsUrl(url: string) {
   return url.replace(regex, '/watch?v=$1')
 }
 
-chrome.runtime.onInstalled.addListener(function () {
-  chrome.contextMenus.create({
-    title: 'No More Shorts',
-    contexts: ['all'],
-    id: 'no-more-shorts',
-    documentUrlPatterns: [
-      'https://*.youtube.com/shorts/*',
-      'http://*.youtube.com/shorts/*',
-    ],
+chrome.contextMenus.create({
+  title: 'No More Shorts',
+  contexts: ['all'],
+  id: 'no-more-shorts',
+  documentUrlPatterns: [
+    'https://*.youtube.com/shorts/*',
+    'http://*.youtube.com/shorts/*',
+  ],
+})
+
+// on context menu click
+chrome.contextMenus.onClicked.addListener(async () => {
+  const [{ url }] = await chrome.tabs.query({
+    active: true,
+    currentWindow: true,
   })
+  const newUrl = convertShortsUrl(url!)
 
-  // on context menu click
-  chrome.contextMenus.onClicked.addListener(async () => {
-    const [{ url }] = await chrome.tabs.query({
-      active: true,
-      currentWindow: true,
-    })
+  chrome.tabs.update({
+    url: newUrl,
+  })
+})
+
+// on page load
+chrome.webRequest.onCompleted.addListener(
+  ({ url }) => {
     const newUrl = convertShortsUrl(url!)
-
     chrome.tabs.update({
       url: newUrl,
     })
-  })
-
-  // on page load
-  chrome.webRequest.onCompleted.addListener(
-    ({ url }) => {
-      const newUrl = convertShortsUrl(url!)
-      chrome.tabs.update({
-        url: newUrl,
-      })
-    },
-    {
-      urls: ['https://*.youtube.com/shorts/*', 'http://*.youtube.com/shorts/*'],
-    }
-  )
-})
+  },
+  {
+    urls: ['https://*.youtube.com/shorts/*', 'http://*.youtube.com/shorts/*'],
+  }
+)
